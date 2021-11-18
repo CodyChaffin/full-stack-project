07f63d8571fc53ea9af26421046bd2ea8fd840f0
cody <cmchaff14@gmail.com>
2021-11-17 18:40:39 -0600

    login form and some components for main page

---
+++ client/src/Components/unauthorizedPage.js
@@ -0,0 +1,14 @@
+import LoginForm from "./LoginForm"
+
+
+function LoginSigninPage(){
+    return (
+        <div>
+         <h1>Log In to your account and start watching today</h1>
+
+            <LoginForm/>
+        </div>
+    )
+}
+    
+export default LoginSigninPage
