cbea68e7f0947aa40598dbb6abab45e17c18cc39
Hashem Gray <Hgray1109@gmail.com>
2021-11-19 08:44:04 -0500

    unauthorizedbackground

--- client/src/Components/unauthorizedPage.js
+++ client/src/Components/unauthorizedPage.js
@@ -1,13 +1,15 @@
 import LoginForm from "./LoginForm"
+import unauthorizedPage from './unauthorizedPage.module.css';
+
 
 
 function LoginSigninPage({setCurrentUser}){
     return (
-        <div className loginPadding>
+        <div className ={unauthorizedPage.body}>
         <img className = "logo" src="/images/project-logo.jpg" alt="" />
          <h1>Create an account or Log In to contine</h1>
             <LoginForm setCurrentUser={setCurrentUser}/>
-       
+    
         </div>
     )
 }
